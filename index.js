340b0a476ee0cd0702c9a547dc5ea9142243717e
Meghansh <meghansh2002@gmail.com>
2023-05-08 21:37:51 +0530

    rectification

--- index.js
+++ index.js
@@ -4,32 +4,29 @@ const { guessInterface } = require("./interfaces/guessageInterface");
 const { jokeInterface } = require("./interfaces/jokeInterface");
 const args = process.argv;
 
-const main = async () => {
-  if (args.length > 2) {
-    switch (args[2]) {
-      case "catfacts":
-        let numfacts = parseInt(args[3]);
-        factGen(true, numfacts);
-        break;
-      case "catfact":
-        factGen(false, 0);
-        break;
-      case "bored":
-        boredInterface();
-        break;
-      case "guessmyage":
-        let name = args[3];
-        guessInterface(name);
-        break;
-      case "joke":
-        jokeInterface();
-        break;
-      case "unzip":
-        console.log(__dirname);
-        break;
-      default:
-        console.error("Unknown command");
-    }
+if (args.length > 2) {
+  switch (args[2]) {
+    case "catfacts":
+      let numfacts = parseInt(args[3]);
+      factGen(true, numfacts);
+      break;
+    case "catfact":
+      factGen(false, 0);
+      break;
+    case "bored":
+      boredInterface();
+      break;
+    case "guessmyage":
+      let name = args[3];
+      guessInterface(name);
+      break;
+    case "joke":
+      jokeInterface();
+      break;
+    case "unzip":
+      console.log(__dirname);
+      break;
+    default:
+      console.error("Unknown command");
   }
-};
-main();
+}
